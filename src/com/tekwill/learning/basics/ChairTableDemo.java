4fe9ce3810b61cc31393e490baaefe346e6581f2
dianabivol <bivol.diana@gmail.com>
2020-11-30 19:43:45 +0200

    solved exercises

---
+++ src/com/tekwill/learning/basics/ChairTableDemo.java
@@ -0,0 +1,12 @@
+package com.tekwill.learning.basics;
+
+public class ChairTableDemo {
+    public static void main(String[] args) {
+        System.out.println("x                       X\n" +
+                           "x                       X\n" +
+                           "x      XXXXXXXXXX       X\n" +
+                           "xxxxx  X         X  xxxxx\n" +
+                           "x   x  X         X  X   x\n" +
+                           "x   x  x         x  x   x\n");
+    }
+}
---
+++ src/com/tekwill/learning/basics/MovieQuoteDetails.java
@@ -0,0 +1,7 @@
+package com.tekwill.learning.basics;
+
+public class MovieQuoteDetails {
+    public static void main(String[] args) {
+        System.out.println("I'll be back!-Teminator-Terminator-2000");
+    }
+}
