63eae8e6c2009c65b6e98275776c5398b67b9d5e
dianabivol <bivol.diana@gmail.com>
2020-12-06 18:43:54 +0200

    addfiles

---
+++ src/com/tekwill/learning/basics/CompleteProgram.java
@@ -0,0 +1,7 @@
+package com.tekwill.learning.basics;
+
+public class CompleteProgram {
+    public static void main(String[] args) {
+        System.out.println("  \\/ \n \\\\// \n\\\\\\/// \n///\\\\\\ \n //\\\\ \n  /\\");
+    }
+}
