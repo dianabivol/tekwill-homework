e32debd27b77b751f9fcb0a44cd973f622fff6c1
dianabivol <bivol.diana@gmail.com>
2020-12-07 00:27:18 +0200

    addfiles

---
+++ src/com/tekwill/learning/basics/FightSong.java
@@ -0,0 +1,26 @@
+package com.tekwill.learning.basics;
+
+public class FightSong {
+    public static void main(String[] args) {
+        System.out.println("Go, team, go!\n" +
+                "You can do it.\n" +
+                "\t\t\t\t\t\t\t\n" +
+                "Go, team, go!\n" +
+                "You can do it.\n" +
+                "you're the best,\n" +
+                "In the West.\n" +
+                "Go, team, go!\n" +
+                "you can do it\n" +
+                "\t\t\t\t\t\t\t\n" +
+                "Go, team, go!\n" +
+                "You can do it.\n" +
+                "you're the best,\n" +
+                "In the West.\n" +
+                "Go, team, go!\n" +
+                "you can do it\n" +
+                "\t\t\t\t\t\t\t\n" +
+                "Go, team, go!\n" +
+                "You can do it.\n");
+    }
+}
+
---
+++ src/com/tekwill/learning/basics/Lanterns.java
@@ -0,0 +1,10 @@
+package com.tekwill.learning.basics;
+
+public class Lanterns {
+    public static void main(String[] args) {
+        System.out.println("Go, team, go!\n" +
+                "You can do it.\n" +
+                "\t\t\t\t\t\t\t\n" +
+                "Go, team, go!\n" +
+
+    }
