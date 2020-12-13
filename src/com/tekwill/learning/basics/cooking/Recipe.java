86245b1b23006ed7fa1d4c9e437939686f21714b
dianabivol <bivol.diana@gmail.com>
2020-12-13 23:18:04 +0200

    addfiles

--- src/com/tekwill/learning/basics/cooking/Recipe.java
+++ src/com/tekwill/learning/basics/cooking/Recipe.java
@@ -1,4 +1,38 @@
 package com.tekwill.learning.basics.cooking;
 
 public class Recipe {
+    public static String sourceUrl = "https://www.test/";
+    public String name;
+    public String info;
+
+    public static void markAsCooked() { System.out.println("Coooooked!");}
+
+    public void update() {System.out.println("Updating");}
+
+    public Recipe(String name, String info) {
+        this.name = name;
+        this.info = info;
+    }
+
+    public static void staticMethod() {
+        markAsCooked();
+        System.out.println(sourceUrl);
+        //update();    cannot access
+    }
+
+    public void instanceMethod() {
+        update();
+        name = "NAME";
+
+        markAsCooked();
+        System.out.println(sourceUrl);
+    }
+
+    @Override
+    public String toString() {
+        return "Recipe {" +
+                "name = " + name +
+                ", info = " + info +
+                "}" ;
+    }
 }
--- src/com/tekwill/learning/basics/kingdom/Knight.java
+++ src/com/tekwill/learning/basics/kingdom/Knight.java
@@ -1,62 +1,22 @@
 package com.tekwill.learning.basics.kingdom;
 
-public class Knight {
-    static String fight = "GF";
-    int age;
-    int nickname;
-    private int guild;
+import static com.tekwill.learning.basics.nonaccessmodifiers.kingdom.Knight.guild;
 
-    public Knight(int age, int nickname, int guild) {
-        this.age = age;
-        this.nickname = nickname;
-        this.guild = guild;
+
+public class KnightDemo {
+    public static void main(String[] args) {
+        Knight knight1 = new Knight(1, "A");
+        Knight knight2 = new Knight(2, "B");
+        Knight knight3 = new Knight(3, "C");
+
+        System.out.println(knight1);
+        System.out.println("knight2.nickname = " + knight2.nickname);
+        System.out.println("knight3.age = " + knight3.age);
+        System.out.println(guild);
+        knight1.fight();
+        knight2.fight();
+        knight2.invokeMagic();
+        System.out.println(guild);
+        Knight.invokeMagic();
     }
-
-    public Knight(int age, int nickname, int guild) {
-        this.age = age;
-        this.nickname = nickname;
-        this.guild = guild;
-    }
-
-    public static void anotherStaticMethod() {
-
-    }
-
-    public static void staticMethod() {
-
-        fight = "GF"; // static fields can be accessed from static method
-        anotherStaticMethod(); // static methods can be accessed from static method
-    }
-
-    public void fightMethod() {
-        staticMethod();Method(); // static can be accessed from instance
-        fight = "GF"; // static can be accessed from instance
-
-        age = 30; // instance can be accessed from instance
-        nickname = 'A'; // instance can be accessed from instance
-        anotherInstanceMethod(); // instance can be accessed from instance
-
-    }
-
-    public void anotherInstanceMethod() {
-
-    }
-
-    public int getAge() {
-        return age;
-    }
-
-    public void setGuild(int guild) {
-        this.guild = guild;
-    }
-
-    @Override
-    public String toString() {
-        return "Knight{" +
-                "age=" + age +
-                ", nickname=" + nickname +
-                ", guild=" + guild +
-                '}';
-    }
-}
 }
